f538550f227e9f72a57923ceb6b4fa15ffbc0f19
Artem Avdyushkin <tofurage@bk.ru>
2025-04-10 20:36:35 +0300

    Done.Module1.Task3-4

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -3,5 +3,6 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.home),
-    path('about', views.about)
+    path('about/', views.about),
+    path('item/<int:item_id>/', views.item_info)
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+items = [
+   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+   {"id": 7, "name": "Картофель фри" ,"quantity":0},
+   {"id": 8, "name": "Кепка" ,"quantity":124},
+]
+
+for item in items:
+    print
+
 user_info = {
     "first_name": "Иван",
     "middle_name": "Петрович",
@@ -25,3 +36,17 @@ def about(request):
         f'Email: {user_info["email"]}'
     )
     
+def item_info(request, item_id):
+    item = None
+    for i in items:
+        if i['id'] == item_id:
+            item = i
+            break
+    if item:
+        return HttpResponse(
+            (f'<h1>{item["name"]}</h1><h2>Количество: {item["quantity"]}</h2>')
+        )
+    else:
+        return HttpResponse(
+            f'<h1>Товар с id={item_id} не найден' 
+        )
