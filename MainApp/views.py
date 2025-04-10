0ac3bef2183eaa0d5e46586f5cad9c337cd6cb21
Artem Avdyushkin <tofurage@bk.ru>
2025-04-10 19:59:45 +0300

    Done. Module1.Task2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+user_info = {
+    "first_name": "Иван",
+    "middle_name": "Петрович",
+    "last_name": "Иванов",
+    "phone": "8-923-600-01-02",
+    "email": "vasya@mail.ru"
+}
 
 def home(request):
     text = """
@@ -9,3 +15,13 @@ def home(request):
     <strong>Автор</strong>: <i>Привет Приветович</i>
     """
     return HttpResponse(text)
+
+def about(request):
+    return HttpResponse(
+        f'Имя: {user_info["first_name"]}<br>'
+        f'Отчество: {user_info["middle_name"]}<br>'
+        f'Фамилия: {user_info["last_name"]}<br>'
+        f'Телефон: {user_info["phone"]}<br>'
+        f'Email: {user_info["email"]}'
+    )
+    
