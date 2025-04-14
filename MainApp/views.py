ccad9e0468894c0f3c39b39bf292550ef2103b33
Artem Avdyushkin <tofurage@bk.ru>
2025-04-14 21:42:45 +0300

    Module3.Part1. Done

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
-items = [
-   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-   {"id": 7, "name": "Картофель фри" ,"quantity":0},
-   {"id": 8, "name": "Кепка" ,"quantity":124},
-]
+# items = [
+#    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+#    {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+#    {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+#    {"id": 7, "name": "Картофель фри" ,"quantity":0},
+#    {"id": 8, "name": "Кепка" ,"quantity":124},
+# ]
 
 def home(request):
     context = {
@@ -27,21 +28,24 @@ def about(request):
     return render(request, "about.html", context)
     
 def item_info(request, item_id:int):
+    # Почитать про .get, это избавит от перебора 
+    items = Item.objects.all()
     item = None
     for i in items:
-        if i['id'] == item_id:
+        if i.id == item_id:
             item = i
             break
     if item:
         context = {
-            "item_name": item["name"],
-            "item_quantity": item["quantity"]
+            "item_name": item.name,
+            "item_count": item.count
         }
         return render(request, "item.html", context)
     return HttpResponse(f'<h2>Товар с id={item_id} не найден</h2> <br> <a href="/items"> Обратно к списку товаров </a>')
 
     
 def all_items(request):
+    items = Item.objects.all()
     context = {
         "items": items
     }
